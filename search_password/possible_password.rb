9cd23114e99c101ee3b48ec96eef71576da5c662
ewi-orion <brpmdr@mailfence.com>
2024-01-26 22:12:39 -0300

    add password file

---
+++ search_password/possible_password.rb
@@ -0,0 +1,24 @@
+def verify_password(user, password_entered, password_file)
+  password = File.readlines(password_file).map(&:chomp)
+
+  if password.include?(password_entered)
+    puts "¡Contraseña correcta para el usuario '#{user}'!"
+  else
+    puts "Contraseña incorrecta para el usuario '#{user}'. Intenta de nuevo."
+  end
+end
+
+# Verificar que se proporcionen los argumentos correctos
+unless ARGV.length == 2
+  puts "Uso: ruby programa.rb <nombre_de_usuario> <password_file>"
+  exit
+end
+
+# Obtener los argumentos de la línea de comandos
+user = ARGV[0]
+password_file = ARGV[1]
+
+puts "Ingrese la contraseña para el usuario '#{user}':"
+password_entered = gets.chomp
+
+verify_password(user, password_entered, password_file)
